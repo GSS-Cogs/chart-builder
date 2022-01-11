20d58f71bf5238ed369c5d8ea74fe94d9910c55b
Waqas Shabbir <wqshabbir@gmail.com>
2022-01-11 15:01:07 +0000

    Added banner as well as declare type definition for missing types

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { TopNav } from "govuk-react";
+import Crown from "@govuk-react/icon-crown";
 
 const Header = (): JSX.Element => {
     return (
-        <div>This is header. Coming soon!</div>
+        <TopNav company={<TopNav.Anchor><TopNav.IconTitle icon={<Crown width="36" height="32" />}>GOV.UK</TopNav.IconTitle></TopNav.Anchor>}>
+        </TopNav>
     )
 }
 export default Header
