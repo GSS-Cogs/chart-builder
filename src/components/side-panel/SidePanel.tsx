e81bd4298966fdcb01c54c5c818787ac92745daf
Charles Hunn <c@hunn.ca>
2022-01-12 23:52:25 +0000

    Create side-panel.css and move relevant styles from App.css
    Also restore example property placeholders and tweak dimensions of side-panel

--- src/components/side-panel/SidePanel.tsx
+++ src/components/side-panel/SidePanel.tsx
@@ -1,46 +1,82 @@
 import { FileUpload } from "govuk-react";
 import Papa from "papaparse";
 import { useState } from "react";
-
+import "./side-panel.css";
 
 const SidePanel = (): JSX.Element => {
-    const [parsedCsvData, setParsedCsvData] = useState([]);
+  const [parsedCsvData, setParsedCsvData] = useState([]);
 
-    const handleUpload = (files: FileList | null) => {
-        files && files.length ? validateData(files[0]) : onFailure("File upload failed");
-    };
+  const handleUpload = (files: FileList | null) => {
+    files && files.length
+      ? validateData(files[0])
+      : onFailure("File upload failed");
+  };
 
-    const validateData = (data: File | string) => {       
-        Papa.parse(data, {
-            worker: true,
-            header: true,
-            skipEmptyLines: true,
-            dynamicTyping: true,
-            complete: function (results) {
-                if (results.errors && results.errors.length > 0) {
-                    onFailure(results.errors[0].message);
-                }
-                else {
-                    console.log("Finished:", results.data);
-                    setParsedCsvData(parsedCsvData);
-                }
-            }
-        })
-    }
+  const validateData = (data: File | string) => {
+    Papa.parse(data, {
+      worker: true,
+      header: true,
+      skipEmptyLines: true,
+      dynamicTyping: true,
+      complete: function (results) {
+        if (results.errors && results.errors.length > 0) {
+          onFailure(results.errors[0].message);
+        } else {
+          console.log("Finished:", results.data);
+          setParsedCsvData(parsedCsvData);
+        }
+      },
+    });
+  };
 
-    const onFailure = (error: string) => {
-        console.log(error);
-    }
-    return (
-        <div id="side-panel">
-            <FileUpload onChange={(e) => handleUpload(e.target.files)}
-                acceptedFormats=".csv"
-                hint="Please upload CSV"
-                name="csv-upload"
-            >
-                Upload a csv
-            </FileUpload>
-        </div>
-    );
+  const onFailure = (error: string) => {
+    console.log(error);
+  };
+  return (
+    <div id="side-panel">
+      <FileUpload
+        onChange={(e) => handleUpload(e.target.files)}
+        acceptedFormats=".csv"
+        name="csv-upload"
+      >
+        Data Source
+      </FileUpload>
+      <br />
+      <br />
+
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+      <p>Example property placeholder</p>
+      <br />
+    </div>
+  );
 };
 export default SidePanel;
