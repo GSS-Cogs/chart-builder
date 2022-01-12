9b14372a2b905553887cc41f7d2940b03069a991
Waqas Shabbir <wqshabbir@gmail.com>
2022-01-12 16:40:29 +0000

    Added logic to parse csv

--- src/components/side-panel/SidePanel.tsx
+++ src/components/side-panel/SidePanel.tsx
@@ -1,33 +1,46 @@
+import { FileUpload } from "govuk-react";
+import Papa from "papaparse";
+import { useState } from "react";
+
+
 const SidePanel = (): JSX.Element => {
-  return (
-    <div id="side-panel">
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-      <p>Config item</p>
-    </div>
-  );
+    const [parsedCsvData, setParsedCsvData] = useState([]);
+
+    const handleUpload = (files: FileList | null) => {
+        files && files.length ? validateData(files[0]) : onFailure("File upload failed");
+    };
+
+    const validateData = (data: File | string) => {       
+        Papa.parse(data, {
+            worker: true,
+            header: true,
+            skipEmptyLines: true,
+            dynamicTyping: true,
+            complete: function (results) {
+                if (results.errors && results.errors.length > 0) {
+                    onFailure(results.errors[0].message);
+                }
+                else {
+                    console.log("Finished:", results.data);
+                    setParsedCsvData(parsedCsvData);
+                }
+            }
+        })
+    }
+
+    const onFailure = (error: string) => {
+        console.log(error);
+    }
+    return (
+        <div id="side-panel">
+            <FileUpload onChange={(e) => handleUpload(e.target.files)}
+                acceptedFormats=".csv"
+                hint="Please upload CSV"
+                name="csv-upload"
+            >
+                Upload a csv
+            </FileUpload>
+        </div>
+    );
 };
 export default SidePanel;
