64464875cfe1b9620ce835c2eac30d726d7acefe
Charles Hunn <charles.hunn@gsscogs.uk>
2022-01-17 22:33:58 +0000

    Minor tweak to the design of the CSV uploader

--- src/components/side-panel/csv-uploader/CSVUploader.tsx
+++ src/components/side-panel/csv-uploader/CSVUploader.tsx
@@ -1,64 +1,58 @@
-
-import { useCallback, useContext, useState } from 'react';
-import { useDropzone } from 'react-dropzone';
+import { useCallback, useContext, useState } from "react";
+import { useDropzone } from "react-dropzone";
 import Papa from "papaparse";
-import "./csv-uploader.css"
-import { ChartContext } from '../../../context/ChartContext';
-import Button from '../../button/Button';
+import "./csv-uploader.css";
+import { ChartContext } from "../../../context/ChartContext";
+import Button from "../../button/Button";
 
 const CSVUploader = (): JSX.Element => {
-    const { setData }: any = useContext(ChartContext);
-    const [selectedFilename, setSelectedFilename] = useState('');
+  const { setData }: any = useContext(ChartContext);
+  const [selectedFilename, setSelectedFilename] = useState("No file selected");
 
-    const onDrop = useCallback((acceptedFiles) => {
-        acceptedFiles.forEach((file: File) => {            
-            validateData(file);
-        })
-    }, []);
-
-    const {
-        getRootProps,
-        getInputProps,
-        open
-    } = useDropzone({
-        onDrop,
-        noClick: true,
-        accept: '.csv',
-        multiple: false,
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file: File) => {
+      validateData(file);
     });
+  }, []);
 
-    const validateData = (data: File) => {
-        Papa.parse(data, {
-            worker: true,
-            header: true,
-            skipEmptyLines: true,
-            dynamicTyping: true,
-            complete: function (results) {
-                if (results.errors && results.errors.length > 0) {
-                    onFailure(results.errors[0].message);
-                } else {
-                    console.log("Finished:", results.data);
-                    setSelectedFilename(data.name);               
-                    setData(results.data);
-                }
-            },
-        });
-    };
+  const { getRootProps, getInputProps, open } = useDropzone({
+    onDrop,
+    noClick: true,
+    accept: ".csv",
+    multiple: false,
+  });
 
-    const onFailure = (error: string) => {
-        console.log(error);
-    };
+  const validateData = (data: File) => {
+    Papa.parse(data, {
+      worker: true,
+      header: true,
+      skipEmptyLines: true,
+      dynamicTyping: true,
+      complete: function (results) {
+        if (results.errors && results.errors.length > 0) {
+          onFailure(results.errors[0].message);
+        } else {
+          console.log("Finished:", results.data);
+          setSelectedFilename(data.name);
+          setData(results.data);
+        }
+      },
+    });
+  };
 
-    return (
-        <div id="csv-uploader">
-            <div {...getRootProps({ id: 'dropzone' })}>
-                <input {...getInputProps()} />
-                <p>{"Drop CSV file"}</p>
-            </div>
-            <Button text={"Choose CSV file"} onClick={open}/>
-            <label id="selected-filename" >{selectedFilename}</label>
-        </div>
+  const onFailure = (error: string) => {
+    console.log(error);
+  };
 
-    );
+  return (
+    <div id="csv-uploader">
+      <label id="selected-filename">{selectedFilename}</label>
+      <div {...getRootProps({ id: "dropzone" })}>
+        <input {...getInputProps()} />
+        <p>{"Drop CSV file"}</p>
+      </div>
+      <Button text={"Choose CSV file"} onClick={open} />
+    </div>
+  );
 };
 export default CSVUploader;
