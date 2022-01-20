d5967468e4743cf476cb150d6b8e8541619711f9
Charles Hunn <charles.hunn@gsscogs.uk>
2022-01-20 01:21:18 +0000

    Improve chart property state management and add further example props

--- src/components/side-panel/SidePanel.tsx
+++ src/components/side-panel/SidePanel.tsx
@@ -1,76 +1,91 @@
 import { useContext } from "react";
 import { ChartContext } from "../../context/ChartContext";
 import CSVUploader from "./csv-uploader/CSVUploader";
+import { camelToSentenceCase } from "../../helper-functions/string-helpers";
 import "./side-panel.css";
 
-// const chartProperties: string[] = [
-//   "Show gridlines",
-//   "Show title",
-//   "Show legend",
-//   "Show x-axis",
-//   "Show y-axis",
-//   "Show x-axis title",
-//   "Show y-axis title",
-// ];
-
 const SidePanel = (): JSX.Element => {
-  const { chartPropertyState, setChartPropertyState }: any =
-    useContext(ChartContext);
+    const { chartProperties, setChartProperties }: any =
+        useContext(ChartContext);
 
-  const handleOnChange = (e: any) => {
-    const value = e.target.checked;
-    const property = e.target.value;
-    if (property === "Show gridlines") {
-      setChartPropertyState({ ...chartPropertyState, showGridLines: value });
-    }
-    if (property === "Show title") {
-      setChartPropertyState({ ...chartPropertyState, showTitle: value });
-    }
-  };
+    const onCheckboxChange = (e: any) => {
+        const { value: property, checked: value } = e.target;
+        setChartProperties({
+            ...chartProperties,
+            [property]: value,
+        });
+    };
 
+    const onTextChange = (e: any) => {
+        const { name: property, value } = e.target;
+        setChartProperties({
+            ...chartProperties,
+            [property]: value,
+        });
+    };
 
-  const getCheckbox = (key: string, value: boolean, index: number) => {
-    return <div className="property-checkbox" key={key}><input
-      type="checkbox"
-      className="checkbox"
-      id={"chartProperty" + index}
-      name={key}
-      value={key}
-      checked={value}
-      onChange={handleOnChange} />
-      <label className="label"
-        htmlFor={"chartProperty" + index}
-      >{key}</label></div>
-  }
+    const getCheckbox = (
+        key: string,
+        value: boolean,
+        index: number,
+        onCheckboxChange: any,
+    ) => {
+        return (
+            <div className="chart-property" key={key}>
+                <input
+                    type="checkbox"
+                    className="checkbox"
+                    id={"chartProperty" + index}
+                    name={key}
+                    value={key}
+                    checked={value}
+                    onChange={onCheckboxChange}
+                />
+                <label className="label" htmlFor={"chartProperty" + index}>
+                    {camelToSentenceCase(key)}
+                </label>
+            </div>
+        );
+    };
 
-  const getTextbox = (key: string, value: string, index: number) => {
-    return <div className="property-checkbox" key={key}><input
-      className="textbox"
-      id={"chartProperty" + index}
-      name={key}
-      value={value}
-      onChange={handleOnChange} />
-    </div>
-  }
+    const getTextbox = (
+        key: string,
+        value: string,
+        index: number,
+        handleOnTextChange: any,
+    ) => {
+        return (
+            <div className="chart-property" key={key}>
+                <label className="label" htmlFor={"chartProperty" + index}>
+                    {camelToSentenceCase(key)}:&nbsp;
+                </label>
+                <div className="property-textbox" key={key}>
+                    <input
+                        className="textbox"
+                        id={"chartProperty" + index}
+                        name={key}
+                        value={value}
+                        onChange={handleOnTextChange}
+                    />
+                </div>
+            </div>
+        );
+    };
 
+    return (
+        <div id="side-panel">
+            <p id="data-source">Data Source</p>
+            <CSVUploader />
 
-  return (
-    <div id="side-panel">
-      <p id="data-source">Data Source</p>
-      <CSVUploader />
-
-      <h3>Configure the Chart</h3>
-      {
-        Object.entries(chartPropertyState).map(([key, value], index) => {
-          if (typeof (value) === "boolean") {
-            return getCheckbox(key, value, index);
-          }
-          else if (typeof (value) === "string") {
-            return getTextbox(key, value, index);
-          }
-        })
-      }
-    </div>
-  );
+            <h3>Configure the Chart</h3>
+            {Object.entries(chartProperties).map(([key, value], index) => {
+                if (typeof value === "boolean") {
+                    return getCheckbox(key, value, index, onCheckboxChange);
+                } else if (typeof value === "string") {
+                    return getTextbox(key, value, index, onTextChange);
+                }
+            })}
+        </div>
+    );
 };
 export default SidePanel;
