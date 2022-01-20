7590b2000850c060c0e765c4c504ff9a67b46b7c
Charles Hunn <charles.hunn@gsscogs.uk>
2022-01-20 01:20:21 +0000

    Install lodash and create camelToSentenceCase string helper function

--- src/helper-functions/string-helpers.ts
+++ src/helper-functions/string-helpers.ts
@@ -1,11 +1,17 @@
+import { upperFirst, startCase } from "lodash";
+
 const titleCase = (str: string) => {
-  return str
-    .toLowerCase()
-    .split("_")
-    .map(function (word) {
-      return word.charAt(0).toUpperCase() + word.slice(1);
-    })
-    .join(" ");
+    return str
+        .toLowerCase()
+        .split("_")
+        .map(function (word) {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+        })
+        .join(" ");
 };
 
-export { titleCase };
+const camelToSentenceCase = (text: string) => {
+    return upperFirst(startCase(text).toLowerCase());
+};
+
+export { titleCase, camelToSentenceCase };
