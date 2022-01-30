37504fc5b8a3ca8cc7c717b6890c92d43484668b
Charles Hunn <charles.hunn@gsscogs.uk>
2022-01-30 17:00:09 +0000

    Remove page intro component - no longer required / to make more space

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Header from "./components/header/Header";
-import PageIntro from "./components/page-intro/PageIntro";
 import ChartContextProvider from "./context/ChartContextProvider";
 import ChartBuilder from "./components/chart-builder/ChartBuilder";
 
@@ -8,12 +7,9 @@ function App() {
   return (
     <div id="app">
       <Header />
-      <div id="main-content">
-        {/* <PageIntro /> */}
-        <ChartContextProvider>
-          <ChartBuilder />
-        </ChartContextProvider>
-      </div>
+      <ChartContextProvider>
+        <ChartBuilder />
+      </ChartContextProvider>
     </div>
   );
 }
