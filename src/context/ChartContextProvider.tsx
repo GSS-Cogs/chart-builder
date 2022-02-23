5848c616383b1bc9dab8d659f26a7e90a1977c0c
Steven Dickinson <sd@tonic.works>
2022-02-23 16:40:10 +0000

    Memoize validateData so it can be included in a useEffect dep array in the volto integration

--- src/context/ChartContextProvider.tsx
+++ src/context/ChartContextProvider.tsx
@@ -1,16 +1,13 @@
 import ChartContext from "./ChartContext";
-import {Dispatch, ReactNode, SetStateAction, useEffect, useState} from "react";
+import {Dispatch, ReactNode, SetStateAction, useCallback, useEffect, useState} from "react";
 import initialChartState from "./initialChartState";
 
 import {arrayColumn, getDistinctValues,} from "../helper-functions/array-helpers";
 
-import {
-  colors,
-  flattenChartProperties,
-} from "../helper-functions/chart-helpers";
+import {colors, flattenChartProperties,} from "../helper-functions/chart-helpers";
 
-import { NO_FILE_SELECTED_TEXT } from "../components/constants/Common-constants";
-import { getConfig } from "@testing-library/react";
+import {NO_FILE_SELECTED_TEXT} from "../components/constants/Common-constants";
+import {getConfig} from "@testing-library/react";
 import getLayout from "../plotly/layout";
 import Papa from "papaparse";
 
@@ -96,23 +93,24 @@ export function useChartCsvData(
     console.log(error);
   };
 
-  const validateData = (data: File | string, filename: string) => {
-    Papa.parse(data, {
-      worker: true,
-      header: true,
-      skipEmptyLines: true,
-      dynamicTyping: true,
-      complete: function (results) {
-        if (results.errors && results.errors.length > 0) {
-          onFailure(results.errors[0].message);
-        } else {
-          setSelectedFilename(filename);
-          setPreviewMode(true);
-          setTidyData(results.data);
-        }
-      },
-    });
-  };
+  const validateData = useCallback(
+      (data: File | string, filename: string) => {
+        Papa.parse(data, {
+          worker: true,
+          header: true,
+          skipEmptyLines: true,
+          dynamicTyping: true,
+          complete: function (results) {
+            if (results.errors && results.errors.length > 0) {
+              onFailure(results.errors[0].message);
+            } else {
+              setSelectedFilename(filename);
+              setPreviewMode(true);
+              setTidyData(results.data);
+            }
+          },
+        });
+  }, [setSelectedFilename, setPreviewMode, setTidyData]);
 
   return {
     validateData,
