540a1722b4c51f88a063b3118f6427b4bf6bf1c8
Charles Hunn <charles.hunn@gsscogs.uk>
2022-03-28 22:44:05 +0100

    Improve flattenChartProperties function in chart-helpers.ts
    - Extract inferBarMode logic out of the function for clean code / single responsibility

--- src/plotly/chartDefinition.ts
+++ src/plotly/chartDefinition.ts
@@ -20,6 +20,13 @@ import {
   flattenChartProperties,
 } from "../helper-functions/chart-helpers";
 
+// A plain 'Bar' chart can also act as grouped bar chart if it
+// has more than one series so we set barmode as 'group'
+// Otherwise if its a 'Stacked Bar' barmode is 'stack'.
+const inferBarMode = (chartType: string) => {
+  return chartType === "bar" ? "group" : "stack";
+};
+
 const updateChartDefinition = (
   chartProperties: any,
   chartData: any,
@@ -28,15 +35,14 @@ const updateChartDefinition = (
 ) => {
   const chartProps = flattenChartProperties(chartProperties);
   const chartType = chartProps.chartType.toLowerCase();
+  chartProps.barmode = inferBarMode(chartType);
 
   let data;
-
   chartType === "map"
     ? (data = getMapData(chartProps, mapData, geoJson))
     : (data = getChartData(chartType, chartProps, chartData));
 
   let layout;
-
   chartType === "map"
     ? (layout = getMapLayout(chartProps))
     : (layout = getChartLayout(chartProps));
@@ -47,7 +53,7 @@ const updateChartDefinition = (
 const getChartData = (chartType: any, chartProps: any, chartData: any) => {
   const traces: any = [];
 
-  //truncate the xSeries values to user specified length
+  // truncate the xSeries values to user specified length
   const xSeries = chartData?.xSeries.values.map((value: string) => {
     return String(value).substring(0, chartProps.xTickLabelMaxLength);
   });
