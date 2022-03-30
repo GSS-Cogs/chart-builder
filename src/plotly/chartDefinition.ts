7f4f051ed4915429f7269edef8e68646f9581f32
Charles Hunn <charles.hunn@gsscogs.uk>
2022-03-30 21:26:09 +0100

    Extract choropleth color scales to dedicated file colorScales.ts

--- src/plotly/chartDefinition.ts
+++ src/plotly/chartDefinition.ts
@@ -1,19 +1,6 @@
 import { getMapLayout, getChartLayout } from "./layout";
 import config from "./config";
-
-const divergingColorScale = [
-  [0, "rgb(242, 108, 49)"],
-  [0.35, "rgb(242, 160, 49)"],
-  [0.5, "rgb(233, 212, 30)"],
-  [0.6, "rgb(202, 233, 30)"],
-  [0.7, "rgb(44, 182, 169)"],
-  [1, "rgb(36, 151, 140)"],
-];
-
-const sequentialColorScale = [
-  [0, "rgb(233, 212, 30)"],
-  [1, "rgb(36, 151, 140)"],
-];
+import { sequentialColorScale, divergingColorScale } from "./colorScales";
 
 import {
   colors,
