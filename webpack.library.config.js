ca15be46ededff6a43989922cb88368d89683f20
Steven Dickinson <steven.dickinson@methods.co.uk>
2022-04-04 11:32:53 +0100

    Compile the assets in production mode
    - was just in development to get the webpack config right

--- webpack.library.config.js
+++ webpack.library.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const baseConfig = {
   entry: './src/library.js',
-  mode: 'development',
+  mode: 'production',
   devtool: 'cheap-source-map',
   module: {
     rules: [
