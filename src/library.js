37d645e513397e143f152ef6fc631d0b6ee414f2
Steven Dickinson <steven.dickinson@methods.co.uk>
2022-04-04 11:30:48 +0100

    Webpack config to create a library version of chart builder

---
+++ src/library.js
@@ -0,0 +1,8 @@
+export * as ChartContext from './context/ChartContext';
+export * as ChartPreview from './components/chart-preview/ChartPreview';
+export {
+  ActualChart
+} from './components/chart-preview/ChartPreview';
+export * as ChartPropertiesSchema from './context/ChartPropertiesSchema';
+export * as SidePanel from './components/side-panel/SidePanel';
+export { useChartContext } from './context/ChartContextProvider';
---
+++ webpack.library.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const baseConfig = {
+  entry: './src/library.js',
+  mode: 'development',
+  devtool: 'cheap-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/typescript', '@babel/react'],
+            plugins: ['@babel/plugin-proposal-optional-chaining']
+          }
+        }],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.wasm', '.tsx', '.ts'],
+  },
+
+
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
+    'react-plotly.js': 'react-plotly.js'
+  },
+
+  optimization: {
+    usedExports: false
+  }
+}
+
+module.exports = [
+  // umd config
+  {
+    ...baseConfig,
+
+    plugins: [
+      new MiniCssExtractPlugin({
+        runtime: false,
+        filename: '../gss-cogs-chart-builder.css'
+      })
+    ],
+    module: {
+      ...baseConfig.module,
+      rules: [
+        ...baseConfig.module.rules,
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        }
+      ]
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist/umd'),
+      libraryTarget: 'umd',
+      filename: 'chart-builder.umd.js',
+      auxiliaryComment: 'chart-builder components'
+    },
+
+  },
+  // cjs config
+  {
+    ...baseConfig,
+
+    module: {
+      ...baseConfig.module,
+      rules: [
+        ...baseConfig.module.rules,
+        {
+          test: /\.css$/,
+          use: 'ignore-loader',
+        }
+      ]
+    },
+
+    output: {
+      path: path.resolve(__dirname, 'dist/cjs'),
+      libraryTarget: 'commonjs2',
+      filename: 'chart-builder.cjs.js',
+      auxiliaryComment: 'chart-builder components'
+    },
+  }
+
+];
