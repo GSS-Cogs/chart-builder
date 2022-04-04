786a297312235ba26f4193160aa6246d4fc9e212
Steven Dickinson <steven.dickinson@methods.co.uk>
2022-04-04 13:50:28 +0100

    Include React in NoDataIcon.svg.tsx
    - in library, since React is an external, makes sure the React.createElement calls refer to the external
    - caused by React 17 not needing the React default import
    - needs a proper fix in the library creation probably

--- src/assets/icons/chart-preview/NoDataIcon.svg.tsx
+++ src/assets/icons/chart-preview/NoDataIcon.svg.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const NoDataIcon = () => {
   return (
     <svg viewBox="0 0 39 35" fill="none" xmlns="http://www.w3.org/2000/svg">
--- src/library.js
+++ src/library.js
@@ -1,8 +1,18 @@
-export * as ChartContext from './context/ChartContext';
-export * as ChartPreview from './components/chart-preview/ChartPreview';
-export {
+import ChartContext from './context/ChartContext';
+import ChartPreview from './components/chart-preview/ChartPreview';
+import {
   ActualChart
 } from './components/chart-preview/ChartPreview';
-export * as ChartPropertiesSchema from './context/ChartPropertiesSchema';
-export * as SidePanel from './components/side-panel/SidePanel';
-export { useChartContext } from './context/ChartContextProvider';
+import  ChartPropertiesSchema from './context/ChartPropertiesSchema';
+import  SidePanel from './components/side-panel/SidePanel';
+import { useChartContext, getInitialChartProperties } from './context/ChartContextProvider';
+
+export {
+  ChartContext,
+  ChartPreview,
+  ActualChart,
+  ChartPropertiesSchema,
+  SidePanel,
+  useChartContext,
+  getInitialChartProperties
+}
