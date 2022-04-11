af40a50000e4a3f1154e0d6ec2e3529321e43518
Charles Hunn <charles.hunn@gsscogs.uk>
2022-04-11 17:11:28 +0100

    Extract map data from sparql input to standalone

--- src/context/StandaloneContextProvider.tsx
+++ src/context/StandaloneContextProvider.tsx
@@ -1,4 +1,6 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useState, useContext, useEffect } from "react";
+import { getMapData } from "../services/map-data/mapDataLoader";
+import ChartContext from "./ChartContext";
 import StandaloneContext from "./StandaloneContext";
 
 interface Props {
@@ -7,9 +9,22 @@ interface Props {
 
 const StandaloneContextProvider = ({ children }: Props): JSX.Element => {
   const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
+  const [sparqlQuery, setSparqlQuery] = useState<string>("");
+  const { loadMapData } = useContext(ChartContext);
+
+  useEffect(() => {
+    if (sparqlQuery === "") return;
+    const populateMap = async () => {
+      const mapData = await getMapData(sparqlQuery);
+      loadMapData(mapData);
+    };
+    populateMap();
+  }, [sparqlQuery, loadMapData]);
 
   return (
-    <StandaloneContext.Provider value={{ isFullScreen, setIsFullScreen }}>
+    <StandaloneContext.Provider
+      value={{ isFullScreen, setIsFullScreen, sparqlQuery, setSparqlQuery }}
+    >
       {children}
     </StandaloneContext.Provider>
   );
