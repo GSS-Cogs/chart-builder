3b1272bbfab86608450ee7e40c2be1ef07a8bf66
Charles Hunn <charles.hunn@gsscogs.uk>
2022-05-23 19:57:55 +0100

    Fix SeriesSelector UX issues and add explanatory comments
    - Handle keyboard navigation of the series list (enter or space to select an item)
    - Improve code readability and add explanatory comments
    - Handle edge cases where the user removes series so the series selector and properties continue to show a valid series

--- src/components/side-panel/property-inputs/SeriesSelector.tsx
+++ src/components/side-panel/property-inputs/SeriesSelector.tsx
@@ -8,23 +8,48 @@ const SeriesSelector: React.VFC = () => {
   const { selectedDimensions: selectedSeries } = useContext(ChartContext);
   const [activeSeries, setActiveSeries] = useState("");
 
-  const isActiveSeriesPresent =
-    selectedSeries.find((s) => s.name === activeSeries) && activeSeries !== "";
+  let isPropertiesOpen = true;
 
+  // When there's only one series set it to be active
   useEffect(() => {
     if (selectedSeries.length === 1) setActiveSeries(selectedSeries[0].name);
   }, []);
 
+  // If there are no series available then there's nothing to show
   if (selectedSeries.length === 0) return null;
+
+  const isActiveSeriesPresent = selectedSeries.some(
+    (s) => s.name === activeSeries,
+  );
+
+  // If the user has removed the active series then set the first series as active
+  if (!isActiveSeriesPresent) {
+    setActiveSeries(selectedSeries[0].name);
+    isPropertiesOpen = false;
+  }
+
+  const handleSelectKeyDown = (e: any) => {
+    switch (e.key) {
+      case "Enter":
+      case " ":
+        setActiveSeries(e.target.value);
+        break;
+    }
+  };
 
   return (
     <div className="property-section">
       <div className="section-heading">Series properties</div>
-      <select id="series-select" size={selectedSeries.length}>
+      <select
+        id="series-select"
+        size={selectedSeries.length}
+        onKeyDown={handleSelectKeyDown}
+      >
         {selectedSeries.map((series, index) => (
           <option
             key={index}
             value={series.name}
+            selected={series.name === activeSeries}
             className="series-option"
             onClick={() => setActiveSeries(series.name)}
           >
@@ -32,9 +57,7 @@ const SeriesSelector: React.VFC = () => {
           </option>
         ))}
       </select>
-      {isActiveSeriesPresent && (
-        <SeriesProperties activeSeries={activeSeries} />
-      )}
+      {isPropertiesOpen && <SeriesProperties activeSeries={activeSeries} />}
     </div>
   );
 };
