369d6c2c0c6310322c1ae2170e72c303d32d847c
Charles Hunn <charles.hunn@gsscogs.uk>
2022-06-13 09:11:20 +0100

    Fix stacked bar chart totals for eeaDataConnector data
    eeaDataConnector data returns y series values as an array of strings, whereas tidyDataConnector returns them as numbers. 
    
    - The fix uses parseInt to convert the strings to numbers in chartDefinition.ts
    - Issue #346 aims to solve the problem at source.

--- src/plotly/chartDefinition.ts
+++ src/plotly/chartDefinition.ts
@@ -71,9 +71,8 @@ const getChartData = (
   chartData?.ySeries.map((series: any, index: number) => {
     // Calculate the Y value totals across all series
     for (let i = 0; i < series.values.length; i++) {
-      totals[i] += series.values[i];
+      totals[i] += parseInt(series.values[i]);
     }
-
     let trace: {};
 
     if (chartProps.orientationProperties.orientation === "horizontal") {
