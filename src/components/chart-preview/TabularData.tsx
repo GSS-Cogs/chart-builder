d14dae42d907f6a0ed8bcc1e0026c223afecb622
StRhys <rhys.st.romaine@gsscogs.uk>
2022-11-16 16:49:17 +0000

    tabular map datta - bring in chartType - get an alternative chart type (for older maps without one?) - logic for map data

--- src/components/chart-preview/TabularData.tsx
+++ src/components/chart-preview/TabularData.tsx
@@ -4,9 +4,11 @@
 import Table from "../table/Table";
 
 const TabularData = ({ chartDefinition, selectedColumns }: any) => {
-  let { data, layout, config } = chartDefinition;
-  console.log(data);
-  let jarray = combineArrays(data[0].x, data[0].y);
+  let { data, layout, config, chartType } = chartDefinition;
+
+  let headers = [];
+  let jarray = [];
+
   function combineArrays(arr1: any, arr2: any) {
     return arr1.map((item: any, i: number) => [item, arr2[i]]);
   }
@@ -17,16 +19,36 @@ const TabularData = ({ chartDefinition, selectedColumns }: any) => {
     }
     return tempArr;
   }
-  if (data.length > 1) {
-    for (let i = 1; i < data.length; i++) {
-      jarray = combineArrays2(jarray, data[i].y);
-    }
-  }
-  console.log(jarray);
 
-  const headers = [selectedColumns?.[0]].concat(
-    data.map((dataset: { name: string }) => dataset.name),
-  );
+  const altChartType = data[0].type; // temp fix for missing chartType
+
+  if (chartType === "map" || altChartType === "choropleth") {
+    jarray = combineArrays(data[0].text, data[0].z);
+
+    let secondColumnHeader = "";
+    if (data[0]?.colorbar?.title?.text) {
+      secondColumnHeader = data[0]?.colorbar?.title?.text;
+    } else if (data[0]?.colorbar?.title.length > 0) {
+      secondColumnHeader = data[0]?.colorbar?.title;
+    }
+    const cleansedSecondColumHeader =
+      secondColumnHeader !== null
+        ? secondColumnHeader.replace(/ *\<[^)]*\> */g, " ")
+        : secondColumnHeader;
+    headers = ["Location", cleansedSecondColumHeader];
+  } else {
+    jarray = combineArrays(data[0].x, data[0].y);
+
+    if (data.length > 1) {
+      for (let i = 1; i < data.length; i++) {
+        jarray = combineArrays2(jarray, data[i].y);
+      }
+    }
+
+    headers = [selectedColumns?.[0]].concat(
+      data.map((dataset: { name: string }) => dataset.name),
+    );
+  }
 
   return (
     <>
